e47a1ba6e56a54a142e67854de16ce9894240ada
carlatakagi <carlatakagi@gmail.com>
2022-06-23 15:48:01 -0400

    adicionando rotas

--- src/api.js
+++ src/api.js
@@ -4,9 +4,14 @@ const express = require('express');
 
 const app = express();
 
+const routes = require('./router');
+const errorHandler = require('./middlewares/error');
+
 app.use(express.json());
 
+app.use(routes);
 // ...
+app.use(errorHandler);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
