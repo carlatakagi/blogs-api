e9925265bc25ecce00d4f7ac8317db18194f907f
carlatakagi <carlatakagi@gmail.com>
2022-06-24 22:29:27 -0400

    alterações

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,10 @@ const userService = require('../services/user.service');
 const { generateJWTToken } = require('../utils/jwt');
 
 const createUser = async (request, response) => {
-  const user = await userService.createUser(request.body);
+  const { displayName, email, password, image } = request.body;
+  const user = await userService.createUser({
+    displayName, email, password, image,
+  });
 
   const token = generateJWTToken(user.dataValues);
 
