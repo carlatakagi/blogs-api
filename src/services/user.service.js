096402b6377af6c3ddbfff3950e93d0d87920f89
carlatakagi <carlatakagi@gmail.com>
2022-06-24 23:36:16 -0400

    adicionando getalluser

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,9 +1,14 @@
 const { User } = require('../database/models');
 
-const createUser = async ({
+const createUser = ({
   displayName, email, password, image,
 }) => User.create({ displayName, email, password, image });
 
+const getAllUsers = () => User.findAll({
+    attributes: { exclude: ['password'] },
+  });
+
 module.exports = {
   createUser,
+  getAllUsers,
 };
