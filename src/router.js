1dcaf8bf5de0df97d53d10544971611097d8761a
carlatakagi <carlatakagi@gmail.com>
2022-06-24 22:10:51 -0400

    alterações nas rotas

--- src/router.js
+++ src/router.js
@@ -4,7 +4,12 @@ const router = express.Router();
 
 const loginController = require('./controllers/login.controller');
 const loginValidation = require('./middlewares/loginValidation');
+const userController = require('./controllers/user.controller');
+const { emailUserValidation,
+  displayNameAndPasswordValidation } = require('./middlewares/userValidation');
 
 router.post('/login', loginValidation, loginController);
+router.post('/user', displayNameAndPasswordValidation,
+emailUserValidation, userController.createUser);
 
 module.exports = router;
