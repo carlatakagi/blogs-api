c396ce6a25abe9e608ab65ec036597da705ebc3c
carlatakagi <carlatakagi@gmail.com>
2022-06-24 23:35:45 -0400

    adicionando rotas

--- src/router.js
+++ src/router.js
@@ -5,11 +5,16 @@ const router = express.Router();
 const loginController = require('./controllers/login.controller');
 const loginValidation = require('./middlewares/loginValidation');
 const userController = require('./controllers/user.controller');
-const { emailUserValidation,
-  displayNameAndPasswordValidation } = require('./middlewares/userValidation');
+const {
+  emailUserValidation,
+  displayNameAndPasswordValidation,
+} = require('./middlewares/userValidation');
+const tokenValidation = require('./middlewares/tokenValidation');
 
 router.post('/login', loginValidation, loginController);
 router.post('/user', displayNameAndPasswordValidation,
 emailUserValidation, userController.createUser);
+
+router.get('/user', tokenValidation, userController.getUser);
 
 module.exports = router;
