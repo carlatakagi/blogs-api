61abc40983cbec4bbd9b0dd6010d9a03dad3629d
carlatakagi <carlatakagi@gmail.com>
2022-06-27 16:54:33 -0400

    adicionando rotas

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const loginController = require('./controllers/login.controller');
 const userController = require('./controllers/user.controller');
 const categoryController = require('./controllers/category.controller');
+const blogPostController = require('./controllers/blogpost.controller');
 const loginValidation = require('./middlewares/loginValidation');
 const {
   emailUserValidation,
@@ -16,9 +17,11 @@ router.post('/login', loginValidation, loginController);
 router.post('/user', displayNameAndPasswordValidation,
 emailUserValidation, userController.createUser);
 router.post('/categories', tokenValidation, categoryController.createCategory);
+router.post('/post', tokenValidation); // 
 
 router.get('/user', tokenValidation, userController.getUser);
 router.get('/user/:id', tokenValidation, userController.getUserById);
 router.get('/categories', tokenValidation, categoryController.getCategories);
+router.get('/post', tokenValidation, blogPostController.getBlogPosts);
 
 module.exports = router;
